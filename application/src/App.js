ee0fee11be62ca0eac6e7556888f9bf8554d1699
Hershal0-0 <hershalrao@gmail.com>
2020-08-16 23:54:49 +0530

    react-components part1

--- application/src/App.js
+++ application/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import  {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from "./components/MenuComponent"
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render()
+  {
+    return(
+    <div>
       <Navbar dark color="primary">
         <div className="container">
-          <NavbarBrand href="/"> Hello Ji</NavbarBrand>
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
