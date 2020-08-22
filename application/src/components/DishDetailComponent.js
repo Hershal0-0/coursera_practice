da140a264e518365fee775fa6288476258ef82cd
Hershal0-0 <hershalrao@gmail.com>
2020-08-22 16:30:00 +0530

    Functional Components

--- application/src/components/DishDetailComponent.js
+++ application/src/components/DishDetailComponent.js
@@ -1,49 +1,39 @@
-import React,{Component} from 'react'
+import React from 'react'
 import {Card,CardImg,CardText,CardTitle} from 'reactstrap';
 
-class DishDetail extends Component{
-
-    
-
-    componentDidMount()
-        {
-            console.log("DishDetail Component componentDidMount invoked")
-        }
-
-    componentDidUpdate(){
-        console.log("DishDetail Cmponent componentDidUpdate invoked ")
+    const renderComments=(dish)=>{
+        const comments=dish.comments.map((comment)=>{
+            // ISO Date to Long Date
+            // const date=new Date(comment.date)
+            // date.toDateString().slice(4,)
+            return(
+                <div key={comment.id} className="padding_left">
+                    <p>{comment.comment}</p><br />
+            <p>--{comment.author},{new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',date:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                </div>
+            )
+        })
+        return comments
     }
-    
 
-    render()
+    function RenderDish({dish})
     {
-        console.log("DishDetail Component render invoked")
-        if(this.props.dish!=null)
+        if(dish!=null)
         {
-            const comment_section=this.props.dish.comments.map((comment)=>{
-                // ISO Date to Long Date
-                // const date=new Date(comment.date)
-                // date.toDateString().slice(4,)
-                return(
-                    <div key={comment.id} className="padding_left">
-                        <p>{comment.comment}</p><br />
-                <p>--{comment.author},{new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',date:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>
-                )
-            })
+            
             return(
                 <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-                        <CardTitle><h3>{this.props.dish.name}</h3></CardTitle>
-                        <CardText>{this.props.dish.description} </CardText>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardTitle><h3>{dish.name}</h3></CardTitle>
+                        <CardText>{dish.description} </CardText>
                     </Card>
                 </div>
                 <div className="col-12 col-md-5 m-1">
                     <Card>
                         <CardTitle><h3 style={{"paddingLeft":"1em"}} >Comments</h3></CardTitle>
-                        <div className="comment" >{comment_section}</div>
+                        <div className="comment" >{renderComments(dish)}</div>
                     </Card>
                 </div>
                 </div>
@@ -56,6 +46,6 @@ class DishDetail extends Component{
         }
     }
 
-}
 
-export default DishDetail
+
+export default RenderDish
